145851d07ffb434d3a26a646651759ef447e7e51
Jared Zwick <52264361+JareddZwick@users.noreply.github.com>
2022-03-12 17:33:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
-from contains import app
+from contains import app, db
 
 # Will start wsgi app by running this script
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
